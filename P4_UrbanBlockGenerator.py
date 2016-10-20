3861e8233a5d4bb464f11dfe6bff7f048eb1c033
Eggimann Sven <sven.eggimann@eawag.ch>
2016-10-20 15:02:40 +0200

    Included GME Environment. Functions so far only with the bug.

--- P4_UrbanBlockGenerator.py
+++ P4_UrbanBlockGenerator.py
@@ -1,3 +1,6 @@
+import subprocess
+from sys import stderr
+from tkMessageBox import IGNORE
 
 # This files creates urban blocksize
 #
@@ -79,7 +82,11 @@ ListWithWWTPCatchments = ["Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fa
 # Read in street network
 
 for pathCatchement in ListWithWWTPCatchments:
-    print("WWTP Catchements: " + str(pathCatchement))
+    
+    # Swiss Coordinate System
+    SwisscordSys = arcpy.SpatialReference(21781) # EPSG:21781 ,CH1903 / LV03
+    
+    print("Path WWTP Catchements: " + str(pathCatchement))
     print("--")
     
     # street_USU   
@@ -87,41 +94,98 @@ for pathCatchement in ListWithWWTPCatchments:
     pathRailwayUSU = str(pathCatchement) + "/" + "railwayNetwork.shp"
     pathSettlementArea = str(pathCatchement) + "/" + "settlementArea.shp"
     
-    print("pathStreetUSU " + str(pathStreetUSU))
-    print("pathRailwayUSU " + str(pathRailwayUSU))
-    print("pathSettlementArea " + str(pathSettlementArea))
+    #print("pathStreetUSU " + str(pathStreetUSU))
+    #print("pathRailwayUSU " + str(pathRailwayUSU))
+    #print("pathSettlementArea " + str(pathSettlementArea))
+    
+    # Define Projection
+    '''arcpy.DefineProjection_management(pathStreetUSU, SwisscordSys)
+    arcpy.DefineProjection_management(pathRailwayUSU, SwisscordSys)
+    arcpy.DefineProjection_management(pathSettlementArea, SwisscordSys)
+    '''
     
     # Merge Street Network & Railroads
-    '''arcpy.env.workspace = pathCatchement
-    arcpy.Merge_management([pathStreetUSU, pathRailwayUSU], pathCatchement + "/" + "streetRailMerge.shp")
-
+    arcpy.env.workspace = pathCatchement
+    #arcpy.Merge_management([pathStreetUSU, pathRailwayUSU], pathCatchement + "/" + "streetRailMerge.shp")
+    
     pathstreetRailMerge = pathCatchement + "/" + "streetRailMerge.shp"
     pathUSU = pathCatchement + "/" + "USU.shp"
     print("Finishing merging Street and Railway Network" + str(pathstreetRailMerge))
-    '''
+    
+    #Assign same coordinate system
+    #print("pathstreetRailMergeFile: " + str(pathstreetRailMerge))
+    #SwisscordSys = arcpy.SpatialReference(21781) # EPSG:21781 ,CH1903 / LV03
+    #arcpy.DefineProjection_management(pathstreetRailMerge, SwisscordSys)
+    #print(arcpy.GetMessages(0))
+    
     # split polygon with lines from tools: 
     # http://www.spatialecology.com/gme/gmedownload.htm
     # http://www.spatialecology.com/gme/geomsplitpolysbylines.htm
     
     # With Split Tool
-    #(Input_Polygons__to_be_split_by_lines_, {Geometry_Type}, Input_Lines__to_split_Polygons_, {Extend_Lines_Limit}, {Extend_to_Extensions}, Output_Polygons__split_by_lines_) 
+    #arcpy.ImportToolbox("//eawag/EA-Daten/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/09-GIS-Python/63-SplitPolyonTool/SplitPolysWithLines/Split_Polygons_with_Lines.tbx")
+    #arcpy.gp.toolbox = "//eawag/EA-Daten/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/09-GIS-Python/63-SplitPolyonTool/SplitPolysWithLines/Split_Polygons_with_Lines.tbx";
+    #arcpy.gp.SplitPolygons(pathSettlementArea, {}, pathstreetRailMerge, {}, pathUSU) 
+    
+    
+    pathToSEGME = r'C:\Users\eggimasv\AppData\Local\Apps\2.0\BN23RV48.G12\9DDGL0P7.2AJ\segm..tion_c23e290fb11c064b_0001.0000_eb3ca996fc44ae0f\SEGME.exe'
     
     
     # With GME
-    os.system(r'C:/InstalledScriptsPaper4/64-GlobalModellingEnvironment/SEGME.exe')  
+    #import subprocess
+    import subprocess as subp
     
-    '''import subprocess
-    subprocess.call(r'C:/InstalledScriptsPaper4/64-GlobalModellingEnvironment/SEGME.exe geom.splitpolysbylines(in=pathSettlementArea, line=pathstreetRailMerge, out=pathUSU')   #-c run(in=\\\"" + newFile + "\\\");');
+    # Trial with .txt (klappt)
+    #subp.call(r'C:/Users/eggimasv/AppData/Local/Apps/2.0/BN23RV48.G12/9DDGL0P7.2AJ/segm..tion_c23e290fb11c064b_0001.0000_eb3ca996fc44ae0f/SEGME.exe -c run(in=\"C:\_scraop\Script.txt\");');
+
+
+    #Works
+    inputCodeBackSlash = r'C:\Users\eggimasv\AppData\Local\Apps\2.0\BN23RV48.G12\9DDGL0P7.2AJ\segm..tion_c23e290fb11c064b_0001.0000_eb3ca996fc44ae0f\SEGME.exe -c geom.splitpolysbylines(in=\"Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\settlementArea.shp\", line=\"Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\streetRailMerge.shp\", out=\"Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\Z_THATSI.shp\");'                             
+    #inputCodeBackSlash = r'C:/Users/eggimasv/AppData/Local/Apps/2.0/BN23RV48.G12/9DDGL0P7.2AJ/segm..tion_c23e290fb11c064b_0001.0000_eb3ca996fc44ae0f/SEGME.exe -c geom.splitpolysbylines(in=\"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/settlementArea.shp\", line=\"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/streetRailMerge.shp\", out=\"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/Z_THATSddI.shp\");'                             
+    #print("inputCodeBackSlash" + str(inputCodeBackSlash))
+    subp.call(inputCodeBackSlash, shell=False)
     
-    GME = r'C:/InstalledScriptsPaper4/64-GlobalModellingEnvironment/SEGME.exe'
-    #ARG = " export.csv(in=" + Input + ", out=" + OutputCSV + '");'
-    ARG = " export.csv(in=" + Input + ", out=" + OutputCSV + '");'
-    geom.splitpolysbylines(in=pathSettlementArea, line=pathstreetRailMerge, out=pathUSU
+    
+    
+    #working on
+    #inputCodeBackSlash = r'C:\Users\eggimasv\AppData\Local\Apps\2.0\BN23RV48.G12\9DDGL0P7.2AJ\segm..tion_c23e290fb11c064b_0001.0000_eb3ca996fc44ae0f\SEGME.exe -c geom.splitpolysbylines(in=\"Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\settlementArea.shp\", line=\"Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\streetRailMerge.shp\", out=\"Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\ZULU_GMEA.shp\");'         
+    #print(inputCodeBackSlash)
+    #subprocess.call(inputCodeBackSlash, shell=False)
+    #subprocess.CalledProcessError(inputCodeBackSlash)
+
+    
+    #print("-------------")
+    
+    #os.system(inputCode)
+    
+    
+    
+    #os.system(r'C:/Users/eggimasv/AppData/Local/Apps/2.0/BN23RV48.G12/9DDGL0P7.2AJ/segm..tion_c23e290fb11c064b_0001.0000_1825d02f9dff539a/SEGME.exe geom.splitpolysbylines(in=pathSettlementArea, line=pathstreetRailMerge, out=pathUSU')  
+    
+    #Does Something
+    #subprocess.call(r'C:/Users/eggimasv/AppData/Local/Apps/2.0/BN23RV48.G12/9DDGL0P7.2AJ/segm..tion_c23e290fb11c064b_0001.0000_1825d02f9dff539a/SEGME.exe geom.splitpolysbylines(in=pathSettlementArea, line=pathstreetRailMerge, out=pathUSU')   #-c run(in=\\\"" + newFile + "\\\");');
+    
+    #subprocess.call(r'C:/Users/eggimasv/AppData/Local/Apps/2.0/BN23RV48.G12/9DDGL0P7.2AJ/segm..tion_c23e290fb11c064b_0001.0000_1825d02f9dff539a/SEGME.exe geom.splitpolysbylines(in="Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/settlementArea.shp", line="Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/street_simplified_split.shp", out="Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/UUUUUUUt.shp");')   #-c run(in=\\\"" + newFile + "\\\");');
+    #subprocess.call(r'C:/Users/eggimasv/AppData/Local/Apps/2.0/BN23RV48.G12/9DDGL0P7.2AJ/segm..tion_c23e290fb11c064b_0001.0000_1825d02f9dff539a/SEGME.exe geom.splitpolysbylines(in=/"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/settlementArea.shp/", line=/"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/street_simplified_split.shp/", out=/"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/UUUUUUUt.shp/");')   #-c run(in=\\\"" + newFile + "\\\");');
+
+    
+    
+    #subprocess.call(r'C:/Users/eggimasv/AppData/Local/Apps/2.0/BN23RV48.G12/9DDGL0P7.2AJ/segm..tion_c23e290fb11c064b_0001.0000_1825d02f9dff539a/SEGME.exe -c ' r'geom.splitpolysbylines(in="Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\settlementArea.shp", line="Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\streetRailMerge.shp", out="Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\ZZZZZ.shp");')
+    print("passed the intersecting")
+    
+            
+        
+        
+    '''GME = r'"C:/Users/eggimasv/AppData/Local/Apps/2.0/BN23RV48.G12/9DDGL0P7.2AJ/segm..tion_c23e290fb11c064b_0001.0000_1825d02f9dff539a/SEGME.exe"'
+    ARG = ' geom.splitpolysbylines(in=r'"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/street_simplified_split.shp"', line=r'"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/street_simplified_split.shp"', out=r'"Q:/Abteilungsprojekte/eng/SWWData/Eggimann_Sven/07-Fallbeispiele/02_GIS_BERN/304_Kallnach/304Kallnach/UUUU.shp"');'
     CMD = GME + ARG
-    '''
     
-    print CMD
+    print("Input Argument")
+    print(CMD)
     subprocess.call(CMD)
+    '''
+    # How it should look like: geom.splitpolysbylines(in="C:\data\plots.shp", line="C:\data\roads.shp", out="C:\data\splitplots.shp");
+    # geom.splitpolysbylines(in="Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN\304_Kallnach\304Kallnach\settlementArea.shp", line="Q:\Abteilungsprojekte\eng\SWWData\Eggimann_Sven\07-Fallbeispiele\02_GIS_BERN
     
     # railway_USU
     # settlement
